3ae7190a5c487254ab706854142d7c6ad900458f
Fredrik Stromberg <fredrik314@gmail.com>
2023-07-17 12:20:18 +0300

    Class for Hilbert Eisenstein series (only implemented for Q(\sqrt(2))

---
+++ packages/maass-forms-hilbert/src/hilbert_maass/all.py
@@ -0,0 +1,6 @@
+from .utils import get_Q_from_bounds, map_int_to_tuple, map_tuple_to_int
+from .functions import bessel_prod, exp_trace_prod
+from .modform.eisenstein_series import HilbertEisensteinSeries
+from .modform.hilbert_maass_space import HilbertMaassFormSpace
+from .modform.hilbert_maass_element import HilbertMaassForm_Element, HilbertMaassForm, \
+    HilbertMaassCoefficients
