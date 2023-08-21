095104cf344169f1f16a516e01764e447769faa8
Fredrik Stromberg <fredrik314@gmail.com>
2023-08-21 23:34:29 +0200

    Add Database representation of the HilbertMaass form element class

---
+++ packages/maass-forms-hilbert/src/hilbert_maass/database/models.py
@@ -0,0 +1,29 @@
+import datetime
+
+import mongoengine as me
+from comp_manager.document.models import DBObjectBase
+
+
+class HilbertMaassFormDB(DBObjectBase):
+    """
+    Hilbert Maass form database object
+    """
+    meta = {
+        'collection': 'hilbert_maass_forms',
+        'object_class_name_base' : 'HilbertMaassForm'
+    }
+    # Properties matching those of HilbertMaassForm_Element
+    # and in particular the output of the 'to_json' method
+    spectral_parameters = me.ListField(me.DictField())
+    coefficients = me.DictField()
+    parent = me.DictField()
+
+    def __str__(self, *args, **kwargs):
+        """
+        String representation of self.
+        """
+        return self.name
+
+    def save(self, **kwargs):
+        pass
+
