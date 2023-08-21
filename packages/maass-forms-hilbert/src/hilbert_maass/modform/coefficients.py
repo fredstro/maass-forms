0e307acff624806de61a1137831a9b0e5063ccf9
Fredrik Stromberg <fredrik314@gmail.com>
2023-08-21 13:14:23 +0200

    Updated the MassFormCoefficients class with bug-fixes and JSON import / export

--- packages/maass-forms-hilbert/src/hilbert_maass/modform/coefficients.py
+++ packages/maass-forms-hilbert/src/hilbert_maass/modform/coefficients.py
@@ -1,3 +1,5 @@
+import json
+
 from hilbert_maass.functions import bessel_prod
 from hilbert_modgroup.upper_half_plane import UpperHalfPlaneProductElement
 from sage.categories.sets_cat import cartesian_product
@@ -5,7 +7,7 @@ from sage.matrix.constructor import matrix
 from sage.matrix.special import diagonal_matrix
 from sage.misc.misc_c import prod
 from sage.modules.free_module_element import vector
-from sage.rings.complex_mpfr import ComplexNumber
+from sage.rings.complex_mpfr import ComplexNumber, ComplexField
 from sage.rings.real_mpfr import RealField, RealNumber
 from typing import ParamSpec
 
@@ -22,7 +24,8 @@ from sage.structure.sage_object import SageObject
 
 from hilbert_maass.functions import bessel_prod, exp_trace_prod
 
-from ..utils import Integer_t, length_from_M, cartesian_product_from_M, dual_ideal_element
+from ..utils import Integer_t, length_from_M, cartesian_product_from_M, dual_ideal_element, \
+    complex_tuple_to_json, complex_tuple_from_json
 
 P = ParamSpec('P')
 log = logging.getLogger(__name__)
@@ -34,8 +37,9 @@ class HilbertMaassCoefficients(SageObject):
 
     """
     def __init__(self, coefficients: Matrix, M: tuple[Integer_t],
-                 coordinate_ideals: list[NumberFieldFractionalIdeal],
-                 form: 'HilbertMaassForm_Element',
+                 spectral_parameter: tuple[ComplexNumber | RealNumber],
+                 space: 'HilbertMaassFormSpace',
+                 coordinate_ideals: list[NumberFieldFractionalIdeal] = None,
                  check: bool = True,
                  **kwargs: P.kwargs) -> None:
         r"""
@@ -45,21 +49,25 @@ class HilbertMaassCoefficients(SageObject):
 
         - ``coefficients`` -- matrix of coefficients
         - ``M`` -- tuple of integers giving the limits used to map the indices of the matrix to tuples
+        - ``spectral_parameter`` -- tuple of complex or real numbers
+        - ``space`` -- Hilbert Maass form space
         - ``coordinate_ideals`` -- list of fractional ideals indexing the different components
-        - ``form`` -- the Maass form
-
+        - ``check`` -- boolean, if True, check that the coefficients are of the correct dimension.
         EXAMPLES::
 
-            sage: from hilbert_maass.all import HilbertMaassForm, HilbertMaassCoefficients
-            sage: K = QuadraticField(2)
-            sage: ideala = K.different()**-1
-            sage: F = HilbertMaassForm(K, (CC(0.5,1),CC(0.5,1)), cuspidal=False)
+            sage: from hilbert_maass.all import HilbertMaassFormSpace, HilbertMaassCoefficients
+            sage: H = HilbertMaassFormSpace(QuadraticField(2), cuspidal=False)
+            sage: spectral_parameter = (CC(0.5,1),CC(0.5,1))
             sage: Cmat = Matrix(RR, [[1],[2],[3],[4],[5],[6],[7],[8],[9]])
-            sage: C = HilbertMaassCoefficients(Cmat, ((-1,1),(-1,1)), [ideala], F); C
-            Coefficients of a Hilbert Maass form with M=((-1, 1), (-1, 1)) and 1 cusps
+            sage: C = HilbertMaassCoefficients(Cmat, ((-1,1),(-1,1)), spectral_parameter, H); C
+            Coefficients of a Hilbert Maass form with M=((-1, 1), (-1, 1)) and 1 cusp
 
         """
         super(HilbertMaassCoefficients, self).__init__(**kwargs)
+        if not coordinate_ideals:
+            representatives = space.group().ideal_cusp_representatives()
+            different = space.group().base_ring().number_field().different()
+            coordinate_ideals = [ideal ** -1 * different ** -1 for ideal in representatives]
         if check:
             if any(M0 == M1 == 0 for M0, M1 in M):
                 raise ValueError('M must be non-zero')
@@ -69,14 +77,120 @@ class HilbertMaassCoefficients(SageObject):
             if coefficients.ncols() != len(coordinate_ideals):
                 raise ValueError('coefficients has number of cols: {coefficients.ncols()}'
                                  f' != {len(coordinate_ideals)}')
+        from hilbert_maass.modform.hilbert_maass_space import HilbertMaassFormSpace
+        if not coordinate_ideals and not space or not isinstance(space, HilbertMaassFormSpace):
+            raise ValueError("Need to either specify coordinate ideals or a Maass form")
+
         self._coordinate_ideals = coordinate_ideals
         self._coefficients = coefficients
         self._M = M
-        self._form = form
+        self._spectral_parameter = spectral_parameter
+        self._space = space
+
+    def to_json(self) -> dict:
+        """
+            JSON representation of self.
+
+            EXAMPLES::
+
+                sage: from hilbert_maass.all import HilbertMaassFormSpace, HilbertMaassCoefficients
+                sage: H = HilbertMaassFormSpace(QuadraticField(2), cuspidal=False)
+                sage: spectral_parameter = (CC(0.5,1),CC(0.5,1))
+                sage: Cmat = Matrix(RR, [[1],[2],[3],[4],[5],[6],[7],[8],[9]])
+                sage: C = HilbertMaassCoefficients(Cmat, ((-1,1),(-1,1)), spectral_parameter, H)
+                sage: C.to_json()
+                {'M': ((-1, 1), (-1, 1)),
+                 'coefficients': [['1.00000000000000'],
+                  ['2.00000000000000'],
+                  ['3.00000000000000'],
+                  ['4.00000000000000'],
+                  ['5.00000000000000'],
+                  ['6.00000000000000'],
+                  ['7.00000000000000'],
+                  ['8.00000000000000'],
+                  ['9.00000000000000']],
+                 'prec': 53,
+                 'space': {'cuspidal': False,
+                  'number_field': {'names': ('a',), 'polynomial': 'x^2 - 2'},
+                  'numerical_precision': 53},
+                 'spectral_parameter': [{'prec': 53,
+                         'val': '0.500000000000000 + 1.00000000000000*I'},
+                        {'prec': 53, 'val': '0.500000000000000 + 1.00000000000000*I'}]}
+        """
+        return {
+            'M': tuple((int(M0[0]), int(M0[1])) for M0 in self._M),
+            'coefficients': [[str(x) for x in r] for r in self._coefficients],
+            'prec': int(self._coefficients.base_ring().prec()),
+            'spectral_parameter': complex_tuple_to_json(self._spectral_parameter),
+            'space': self._space.to_json()
+        }
+
+    @classmethod
+    def from_json(cls, data: str | dict) -> 'HilbertMaassCoefficients':
+        """
+            Create an instance of HilbertMaassCoefficients from json formatted dict or string.
+
+            EXAMPLES::
+
+                sage: from hilbert_maass.all import HilbertMaassFormSpace, HilbertMaassCoefficients
+                sage: H = HilbertMaassFormSpace(QuadraticField(2), cuspidal=False)
+                sage: spectral_parameter = (CC(0.5,1),CC(0.5,1))
+                sage: Cmat = Matrix(RR, [[1],[2],[3],[4],[5],[6],[7],[8],[9]])
+                sage: C = HilbertMaassCoefficients(Cmat, ((-1,1),(-1,1)), spectral_parameter, H)
+                sage: HilbertMaassCoefficients.from_json(C.to_json()) == C
+                True
+                sage: import json
+                sage: json_string = json.dumps(C.to_json())
+                sage: HilbertMaassCoefficients.from_json(json_string) == C
+                True
+
+        """
+        if isinstance(data, str):
+            data = json.loads(data)
+        CF = ComplexField(data['prec'])
+        coefficients = matrix(CF, data['coefficients'])
+        from hilbert_maass.modform.hilbert_maass_space import HilbertMaassFormSpace
+        return cls(
+            coefficients, M=data['M'],
+            spectral_parameter=complex_tuple_from_json(data['spectral_parameter']),
+            space=HilbertMaassFormSpace.from_json(data['space'])
+        )
+
+    def space(self):
+        return self._space
+
+    def coefficient_matrix(self):
+        return self._coefficients
+
+    def spectral_parameter(self):
+        return self._spectral_parameter
+
+    def __eq__(self, other):
+        if not isinstance(other, HilbertMaassCoefficients):
+            return False
+        return self.space() == other.space() and \
+            self.coefficient_matrix() == other.coefficient_matrix() and \
+            self.spectral_parameter() == other.spectral_parameter()
 
     def __repr__(self):
+        """
+        String representation of self.
+
+        EXAMPLES::
+
+            sage: from hilbert_maass.all import HilbertMaassFormSpace, HilbertMaassCoefficients
+            sage: H = HilbertMaassFormSpace(QuadraticField(2), cuspidal=False)
+            sage: spectral_parameter = (CC(0.5,1),CC(0.5,1))
+            sage: Cmat = Matrix(RR, [[1],[2],[3],[4],[5],[6],[7],[8],[9]])
+            sage: C = HilbertMaassCoefficients(Cmat, ((-1,1),(-1,1)), spectral_parameter, H)
+            sage: C.__repr__()
+            'Coefficients of a Hilbert Maass form with M=((-1, 1), (-1, 1)) and 1 cusp'
+
+
+        """
+        num_cusps = len(self._coordinate_ideals)
         return f"Coefficients of a Hilbert Maass form with M={self._M} and" \
-               f" {len(self._coordinate_ideals)} cusps"
+               f" {num_cusps} cusp{'s' if num_cusps > 1 else ''}"
 
     def __getitem__(self, key: tuple | Integer_t) -> ComplexNumber:
         """
@@ -84,12 +198,11 @@ class HilbertMaassCoefficients(SageObject):
 
         EXAMPLES::
 
-            sage: from hilbert_maass.all import HilbertMaassForm, HilbertMaassCoefficients
-            sage: K = QuadraticField(2)
-            sage: ideala = K.different()**-1
-            sage: F = HilbertMaassForm(K, (CC(0.5,1),CC(0.5,1)), cuspidal=False)
+            sage: from hilbert_maass.all import HilbertMaassFormSpace, HilbertMaassCoefficients
+            sage: H = HilbertMaassFormSpace(QuadraticField(2), cuspidal=False)
+            sage: spectral_parameter = (CC(0.5,1),CC(0.5,1))
             sage: Cmat = Matrix(RR, [[1],[2],[3],[4],[5],[6],[7],[8],[9]])
-            sage: C = HilbertMaassCoefficients(Cmat, ((-1,1),(-1,1)), [ideala], F)
+            sage: C = HilbertMaassCoefficients(Cmat, ((-1,1),(-1,1)), spectral_parameter, H)
             sage: C[0]
             1.00000000000000
             sage: C[0, 1]
@@ -117,6 +230,9 @@ class HilbertMaassCoefficients(SageObject):
         if isinstance(key, tuple) and len(key) == 2 and isinstance(key[1], tuple):
             cusp, v = key
         elif isinstance(key, (int, Integer)) or isinstance(key, tuple):
+            v = key
+        elif isinstance(key, NumberFieldElement):
+            cusp = 0
             v = key
         if cusp < 0 or cusp >= len(self._coordinate_ideals):
             raise ValueError(f"Cusp index {cusp} out of bounds.")
@@ -191,13 +307,11 @@ def get_pb_pts(P, Q: tuple, ideala: NumberFieldFractionalIdeal, Y: tuple, prec: 
 
 
 def compute_coefficients(space: 'HilbertMaassFormSpace',
-                         s: tuple[complex | ComplexNumber],
+                         spectral_parameter: tuple[complex | ComplexNumber],
                          ideala: NumberFieldFractionalIdeal = None,
                          idealb: NumberFieldFractionalIdeal = None,
                          M: tuple[Integer_t] = None,
                          Y: tuple[float | RealNumber] = None,
-                         form: 'HilbertMaassform_Element' = None,
-                         prec: int = 53,
                          sgn: str = '-', returnV: bool = False) -> 'HilbertMaassCoefficients':
     r"""
 
@@ -222,12 +336,12 @@ def compute_coefficients(space: 'HilbertMaassFormSpace',
         sage: s = CC(1.5,1.5), CC(1.5,1.5)
         sage: H = HilbertMaassFormSpace(QuadraticField(2), cuspidal=False)
         sage: X= compute_coefficients(H, s, M = 2); X
-        Coefficients of a Hilbert Maass form with M=((-2, 2), (-2, 2)) and 1 cusps
+        Coefficients of a Hilbert Maass form with M=((-2, 2), (-2, 2)) and 1 cusp
     """
-    complex_field = s[0].parent()
+    complex_field = spectral_parameter[0].parent()
     ideala = ideala or space.number_field().ideal(1)
     idealb = idealb or space.number_field().ideal(1)
-    smax = max(ceil(abs(s0)) for s0 in s)
+    smax = max(ceil(abs(s0)) for s0 in spectral_parameter)
     zpb, zm, Qs, M, Y = get_pb_pts_set_params(space, smax=smax,
                                               M=M, Y=Y, ideala=ideala)
     log.debug(f"M = {M}, Y = {Y}, Qs = {Qs}")
@@ -240,8 +354,9 @@ def compute_coefficients(space: 'HilbertMaassFormSpace',
                 matrixV[(V, W)] = 0
             else:
                 w = dual_ideal_element(W, idealb)
-                matrixV[(V, W)] = matrix_element(s, Qs, v, w, zpb, zm, sgn='-')
-        matrixV[(V, V)] = matrixV[(V, V)] - bessel_prod(tuple(v), tuple(Y), s, sgn='-')
+                matrixV[(V, W)] = matrix_element(spectral_parameter, Qs, v, w, zpb, zm, sgn='-')
+        matrixV[(V, V)] = matrixV[(V, V)] - bessel_prod(tuple(v), tuple(Y), spectral_parameter,
+                                                        sgn='-')
     RHS = {}
     # if is_cuspidal:
     # Set c(0)=0 and c(delta)=1 where delta >>0 is generator of the index ideal.
@@ -250,9 +365,16 @@ def compute_coefficients(space: 'HilbertMaassFormSpace',
     # tuple for 1
     # Try this first:
     ideala_dual = ideala ** -1 * ideala.number_field().different() ** -1
-    delta = sum(ideala_dual.gens())
-    if not delta.is_totally_positive() or \
-            ideala_dual != ideala_dual.number_field().fractional_ideal(delta):
+    x, y = ideala_dual.gens_two()
+    delta = None
+    for delta_test in [x + y, x - y, -x - y, -x + y]:
+        if not delta_test.is_totally_positive():
+            continue
+        if ideala_dual != ideala_dual.number_field().fractional_ideal(delta_test):
+            continue
+        delta = delta_test
+        break
+    if not delta:
         raise ArithmeticError(f"Cannot find a totally positive generator for {ideala_dual}")
     # Coordinate vector of delta
     t_1 = ideal_coordinates(ideala_dual, delta)
@@ -262,9 +384,9 @@ def compute_coefficients(space: 'HilbertMaassFormSpace',
             V = tuple(V)
             v = dual_ideal_element(V, ideala)
             W = w = (0,) * len(v)
-            RHS[(V, W)] = matrix_element(s, Qs, v, w, zpb, zm, sgn='+')
+            RHS[(V, W)] = matrix_element(spectral_parameter, Qs, v, w, zpb, zm, sgn='+')
             if V == W:  # == 0,...,0
-                RHS[(V, W)] = RHS[(V, W)] - bessel_prod(v, tuple(Y), s, sgn='+')
+                RHS[(V, W)] = RHS[(V, W)] - bessel_prod(v, tuple(Y), spectral_parameter, sgn='+')
     else:
         for V in cartesian_product_from_M(M):
             RHS[(V, t_1)] = matrixV[(V, t_1)]
@@ -285,6 +407,12 @@ def compute_coefficients(space: 'HilbertMaassFormSpace',
     if returnV:
         return Vmat, RHSmat
     log.debug(f"n, n_0, n_1= {n, n_0, n_1}")
+    # Make sure that n0 < n1:
+    c0 = complex_field(0)
+    c1 = complex_field(1)
+    if n_0 > n_1:
+        n_0, n_1 = n_1, n_0
+        c0, c1 = c1, c0
     if space.is_cuspidal():
         delete_rows = (n_0, n_1)
         Vmat = Vmat.delete_rows(delete_rows)
@@ -292,14 +420,13 @@ def compute_coefficients(space: 'HilbertMaassFormSpace',
         Vmat = Vmat.delete_columns(delete_rows)
         X = Vmat.solve_right(-RHSmat)
         # Add back coefficients for 0 and 1
-        rows = X.rows()[0:n_0] + [(complex_field(0),)] + X.rows()[n_0:n_1 - 1] \
-                               + [(complex_field(1),)] + X.rows()[n_1 - 1:]
+        rows = X.rows()[0:n_0] + [(c0,)] + X.rows()[n_0:n_1 - 1] \
+                               + [(c1,)] + X.rows()[n_1 - 1:]
         X = matrix(rows)
     else:
         X = Vmat.solve_right(-RHSmat)
-    if not form:
-        form = space(s)
-    return HilbertMaassCoefficients(X, M, coordinate_ideals=space.dual_ideals(), form=form)
+    return HilbertMaassCoefficients(X, M, spectral_parameter=spectral_parameter,
+                                    space=space, coordinate_ideals=space.dual_ideals())
 
 
 def matrix_element(s: tuple, Q: tuple, v: tuple, w: tuple, zpb_v: list, zm_v: list,
