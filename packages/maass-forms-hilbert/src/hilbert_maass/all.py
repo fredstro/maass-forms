7ee7e68ac9dae95f98d4240062431b53a46074c2
Fredrik Stromberg <fredrik314@gmail.com>
2023-09-21 17:45:50 +0100

    Added functions for searching to the all import

--- packages/maass-forms-hilbert/src/hilbert_maass/all.py
+++ packages/maass-forms-hilbert/src/hilbert_maass/all.py
@@ -6,6 +6,7 @@ from .modform.eisenstein_series import HilbertEisensteinSeries
 from .modform.hilbert_maass_space import HilbertMaassFormSpace
 from .modform.hilbert_maass_element import HilbertMaassForm_Element, HilbertMaassForm
 from .modform.coefficients import HilbertMaassCoefficients, compute_coefficients, get_pb_pts
+from .search.search import compute_on_grid, create_grid, compute_one_spectral_parameter
 try:
     from .database.models import HilbertMaassFormDB
 except ImportError as e:
