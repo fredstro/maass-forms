12ccf9217c9a2af83ea36b38c4d753a89bcd329e
Fredrik Stromberg <fredrik314@gmail.com>
2023-10-19 12:47:05 +0100

    Changed imports in doctests

--- packages/maass-forms-klein/src/knot_maass/hyperbolic_space/kleinian_group.py
+++ packages/maass-forms-klein/src/knot_maass/hyperbolic_space/kleinian_group.py
@@ -5,24 +5,40 @@ acting discretely on the upper half-space.
 """
 from knot_maass.modform.upper_half_space.pyx import UpperHalfSpaceElement__class
 from sage.all import ZZ
-from sage.groups.matrix_gps.linear import LinearMatrixGroup_generic
+from sage.groups.matrix_gps.matrix_group import MatrixGroup_generic
 from sage.rings.integer import Integer
 from sage.rings.number_field.number_field import QuadraticField
 from sage.rings.number_field.order import Order
 
 
-class KleinianGroup_class(LinearMatrixGroup_generic):
+class KleinianGroup_class(MatrixGroup_generic):
     r"""
     Class representing a Kleinian group, i.e. a discrete subgroup of PSL(2,C)
     acting discretely on the upper half-space.
 
     TODO: write examples.
+
     """
     def __init__(self, x, **kwargs):
+        r"""
+
+        INPUT:
+        - ``x`` -- integer, order in imaginary quadratic number field, list of generators
+
+        """
+        MS = MatrixSpace(QQbar, 2, 2)
         if isinstance(x, (int, Integer)) and x < 0 and ZZ(x).is_fundamental_discriminant():
-            x = QuadraticField(x).ring_of_integers()
+            K = QuadraticField(x)
+            if K.class_number() > 1:
+                raise NotImplementedError("Only class number 1 is supported.")
+            x = QuadraticField(x).ring_of_integers().gens()
+            for b in K.basis():
+                gens.append()
         if isinstance(x, Order):
             LinearMatrixGroup_generic.__init__(self, x, **kwargs)
+        elif isinstance(x, list):
+            # x is a list of generators
+
         else:
             raise NotImplementedError
 
