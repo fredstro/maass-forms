d4053b4ae9e8edbe58237377963005e8a5ca0a2e
Fredrik Stromberg <fredrik.stromberg@wincon.co.uk>
2026-04-06 20:00:39 +0100

    chore: configure pyproject.toml and setup.py and Makefile for all 3 packages in sync

--- packages/maass-forms-hilbert/setup.py
+++ packages/maass-forms-hilbert/setup.py
@@ -1,83 +1,92 @@
 import os
 import shutil
 import subprocess
+import sys
 import sysconfig
 from contextlib import contextmanager
 
 import setuptools
-from Cython.Build import cythonize
-from setuptools.extension import Extension
 
-# Find correct value for SAGE_LIB which is needed to compile the Cython extensions.
+# Detect SageMath/PassageMath. Cython extensions require it, but we allow
+# installation without it so that the pure-Python parts remain usable.
+SAGE_LIB = None
 try:
     from sage.env import SAGE_LIB
 except ModuleNotFoundError:
     SAGE_LIB = os.getenv("SAGE_LIB") or sysconfig.get_path("purelib")
-# Check if sage is installed in SAGE_LIB
-if not os.path.isdir(SAGE_LIB + "/sage"):
-    raise ModuleNotFoundError("Sagemath or passagemath needs to be installed.")
 
-INCLUDE_DIRS = []
-LIBRARY_DIRS = []
-if shutil.which("brew") is not None:
-    proc = subprocess.Popen(
-        "brew --prefix",
-        shell=True,
-        stdout=subprocess.PIPE,
-        stdin=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        close_fds=True,
-    )
-    HOMEBREW_PREFIX = proc.stdout.readline().decode("utf-8").strip()
-    HOMEBREW_LIB = HOMEBREW_PREFIX + "/lib"
-    LIBRARY_DIRS.append(HOMEBREW_LIB)
-    HOMEBREW_INC = HOMEBREW_PREFIX + "/include"
-    INCLUDE_DIRS.append(HOMEBREW_INC)
+HAS_SAGE = SAGE_LIB is not None and os.path.isdir(SAGE_LIB + "/sage")
 
-# Extension modules using Cython
-extra_compile_args = [
-    "-Wno-unused-function",
-    "-Wno-implicit-function-declaration",
-    "-Wno-unused-variable",
-    "-Wno-deprecated-declarations",
-    "-Wno-deprecated-register",
-]
+if HAS_SAGE:
+    from Cython.Build import cythonize
+    from setuptools.extension import Extension
 
-# Find maass_form_core source for cimport resolution
-try:
-    import maass_form_core
-    CORE_SRC = os.path.dirname(os.path.dirname(maass_form_core.__file__))
-except ImportError:
-    CORE_SRC = os.path.join(os.path.dirname(__file__), "..", "maass-form-core", "src")
+    INCLUDE_DIRS = []
+    LIBRARY_DIRS = []
+    if shutil.which("brew") is not None:
+        proc = subprocess.Popen(
+            "brew --prefix",
+            shell=True,
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            close_fds=True,
+        )
+        HOMEBREW_PREFIX = proc.stdout.readline().decode("utf-8").strip()
+        HOMEBREW_LIB = HOMEBREW_PREFIX + "/lib"
+        LIBRARY_DIRS.append(HOMEBREW_LIB)
+        HOMEBREW_INC = HOMEBREW_PREFIX + "/include"
+        INCLUDE_DIRS.append(HOMEBREW_INC)
 
-ext_modules = [
-    Extension(
-        "maass_forms_hilbert.functions.functions_cy",
-        ["src/maass_forms_hilbert/functions/functions_cy.pyx"],
-        include_dirs=INCLUDE_DIRS,
-        extra_compile_args=extra_compile_args,
-        library_dirs=LIBRARY_DIRS,
-    ),
-]
+    extra_compile_args = [
+        "-Wno-unused-function",
+        "-Wno-implicit-function-declaration",
+        "-Wno-unused-variable",
+        "-Wno-deprecated-declarations",
+        "-Wno-deprecated-register",
+    ]
 
-try:
-    from sage.misc.package_dir import cython_namespace_package_support
-except ImportError:
+    # Find maass_form_core source for cimport resolution
+    try:
+        import maass_form_core
+        CORE_SRC = os.path.dirname(os.path.dirname(maass_form_core.__file__))
+    except ImportError:
+        CORE_SRC = os.path.join(os.path.dirname(__file__), "..", "maass-form-core", "src")
 
-    @contextmanager
-    def cython_namespace_package_support():
-        yield
-
-
-with cython_namespace_package_support():
-    setuptools.setup(
-        ext_modules=cythonize(
-            ext_modules,
-            include_path=["src", SAGE_LIB, CORE_SRC],
-            compiler_directives={
-                "embedsignature": True,
-                "language_level": "3",
-            },
-            annotate=True,
+    ext_modules = [
+        Extension(
+            "maass_forms_hilbert.functions.functions_cy",
+            ["src/maass_forms_hilbert/functions/functions_cy.pyx"],
+            include_dirs=INCLUDE_DIRS,
+            extra_compile_args=extra_compile_args,
+            library_dirs=LIBRARY_DIRS,
         ),
+    ]
+
+    try:
+        from sage.misc.package_dir import cython_namespace_package_support
+    except ImportError:
+
+        @contextmanager
+        def cython_namespace_package_support():
+            yield
+
+    with cython_namespace_package_support():
+        setuptools.setup(
+            ext_modules=cythonize(
+                ext_modules,
+                include_path=["src", SAGE_LIB, CORE_SRC],
+                compiler_directives={
+                    "embedsignature": True,
+                    "language_level": "3",
+                },
+                annotate=True,
+            ),
+        )
+else:
+    print(
+        "WARNING: SageMath/PassageMath not found. "
+        "Installing without Cython extensions.",
+        file=sys.stderr,
     )
+    setuptools.setup()
--- packages/maass-forms-klein/setup.py
+++ packages/maass-forms-klein/setup.py
@@ -1,80 +1,92 @@
 import os
 import shutil
 import subprocess
+import sys
 import setuptools
 import sysconfig
 from contextlib import contextmanager
-from setuptools.extension import Extension
-from Cython.Build import cythonize
 
-# Find correct value for SAGE_LIB which is needed to compile the Cython extensions.
+# Detect SageMath/PassageMath. Cython extensions require it, but we allow
+# installation without it so that the pure-Python parts remain usable.
+SAGE_LIB = None
 try:
     from sage.env import SAGE_LIB
 except ModuleNotFoundError:
     SAGE_LIB = os.getenv("SAGE_LIB") or sysconfig.get_path("purelib")
-# Check if sage is installed in SAGE_LIB
-if not os.path.isdir(SAGE_LIB + "/sage"):
-    raise ModuleNotFoundError("Sagemath or passagemath needs to be installed.")
 
-INCLUDE_DIRS = []
-LIBRARY_DIRS = []
-if shutil.which("brew") is not None:
-    proc = subprocess.Popen(
-        "brew --prefix",
-        shell=True,
-        stdout=subprocess.PIPE,
-        stdin=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        close_fds=True,
-    )
-    HOMEBREW_PREFIX = proc.stdout.readline().decode("utf-8").strip()
-    HOMEBREW_LIB = HOMEBREW_PREFIX + "/lib"
-    LIBRARY_DIRS.append(HOMEBREW_LIB)
-    HOMEBREW_INC = HOMEBREW_PREFIX + "/include"
-    INCLUDE_DIRS.append(HOMEBREW_INC)
+HAS_SAGE = SAGE_LIB is not None and os.path.isdir(SAGE_LIB + "/sage")
 
-# Extension modules using Cython
-extra_compile_args = [
-    "-Wno-unused-function",
-    "-Wno-implicit-function-declaration",
-    "-Wno-unused-variable",
-    "-Wno-deprecated-declarations",
-    "-Wno-deprecated-register",
+PACKAGES = [
+    "maass_forms_klein",
+    "maass_forms_klein.functions",
+    "maass_forms_klein.database",
+    "maass_forms_klein.utils",
+    "maass_forms_klein.hyperbolic_space",
+    "maass_forms_klein.modform",
 ]
 
-ext_modules = [
-    Extension(
-        'maass_forms_klein.hyperbolic_space.upper_half_space',
-        sources=[os.path.join('src/maass_forms_klein/hyperbolic_space/upper_half_space.pyx')],
-        extra_compile_args=extra_compile_args,
-        include_dirs=INCLUDE_DIRS, library_dirs=LIBRARY_DIRS
+if HAS_SAGE:
+    from Cython.Build import cythonize
+    from setuptools.extension import Extension
+
+    INCLUDE_DIRS = []
+    LIBRARY_DIRS = []
+    if shutil.which("brew") is not None:
+        proc = subprocess.Popen(
+            "brew --prefix",
+            shell=True,
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            close_fds=True,
+        )
+        HOMEBREW_PREFIX = proc.stdout.readline().decode("utf-8").strip()
+        HOMEBREW_LIB = HOMEBREW_PREFIX + "/lib"
+        LIBRARY_DIRS.append(HOMEBREW_LIB)
+        HOMEBREW_INC = HOMEBREW_PREFIX + "/include"
+        INCLUDE_DIRS.append(HOMEBREW_INC)
+
+    extra_compile_args = [
+        "-Wno-unused-function",
+        "-Wno-implicit-function-declaration",
+        "-Wno-unused-variable",
+        "-Wno-deprecated-declarations",
+        "-Wno-deprecated-register",
+    ]
+
+    ext_modules = [
+        Extension(
+            "maass_forms_klein.hyperbolic_space.upper_half_space",
+            sources=[os.path.join("src/maass_forms_klein/hyperbolic_space/upper_half_space.pyx")],
+            extra_compile_args=extra_compile_args,
+            include_dirs=INCLUDE_DIRS, library_dirs=LIBRARY_DIRS
+        )
+    ]
+
+    try:
+        from sage.misc.package_dir import cython_namespace_package_support
+    except ImportError:
+
+        @contextmanager
+        def cython_namespace_package_support():
+            yield
+
+    with cython_namespace_package_support():
+        setuptools.setup(
+            packages=PACKAGES,
+            ext_modules=cythonize(
+                ext_modules,
+                include_path=["src", SAGE_LIB],
+                compiler_directives={
+                    "embedsignature": True,
+                    "language_level": "3",
+                },
+            ),
+        )
+else:
+    print(
+        "WARNING: SageMath/PassageMath not found. "
+        "Installing without Cython extensions.",
+        file=sys.stderr,
     )
-]
-
-print("ext modules=",ext_modules)
-try:
-    from sage.misc.package_dir import cython_namespace_package_support
-except ImportError:
-
-    @contextmanager
-    def cython_namespace_package_support():
-        yield
-
-
-with cython_namespace_package_support():
-    setuptools.setup(
-        packages=['maass_forms_klein',
-                  'maass_forms_klein.functions',
-                  'maass_forms_klein.database',
-                  'maass_forms_klein.utils',
-                  'maass_forms_klein.hyperbolic_space',
-                  'maass_forms_klein.modform'],
-        ext_modules=cythonize(
-            ext_modules,
-            include_path=['src', SAGE_LIB],
-            compiler_directives={
-                'embedsignature': True,
-                'language_level': '3',
-            },
-        ),
-    )
+    setuptools.setup(packages=PACKAGES)
