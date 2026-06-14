4027977c70987c40fd772a2ca667c7421420dcf1
Fredrik Stromberg <fredrik.stromberg@wincon.co.uk>
2026-06-14 17:44:04 +0200

    test: update filterwarnings to capture starlette deprecation warnings

--- packages/maass-form-core/src/maass_form_core/database/queryset.py
+++ packages/maass-form-core/src/maass_form_core/database/queryset.py
@@ -25,7 +25,7 @@ class MaassFormQuerySet(QuerySetCompat):
     EXAMPLES::
 
         sage: import warnings
-        sage: warnings.filterwarnings('ignore', category=DeprecationWarning)
+        sage: warnings.filterwarnings('ignore', category=Warning)
         sage: from maass_form_core.database.queryset import MaassFormQuerySet
         sage: MaassFormQuerySet  # class exists
         <class 'maass_form_core.database.queryset.MaassFormQuerySet'>
